848663c228f951067bc3ef3de47e721cec7643a4
StefanButacu <stefbutacu@gmail.com>
2021-12-19 12:37:04 +0200

    First commit in events

---
+++ src/main/java/com/toysocialnetworkgui/domain/Event.java
@@ -0,0 +1,12 @@
+package com.toysocialnetworkgui.domain;
+
+/**
+ * Class that manages an event. I think it should be a subject where users are observers to this object.
+ * If it passed a day/(an hour for testing purposes) from the last notification you should notify all users.
+ * Users have to implement the observer interface
+ * Events has: - A name, a description, DateLocalTime start, DateLocalTime end, Participants(Observers)
+ *
+ */
+public class Event {
+
+}
