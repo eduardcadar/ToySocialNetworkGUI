a41471ae45b47dabfde9e6be1599b25ba561bcfc
StefanButacu <stefbutacu@gmail.com>
2021-12-29 13:22:19 +0200

    COnflict solve

--- src/main/java/com/toysocialnetworkgui/utils/CONSTANTS.java
+++ src/main/java/com/toysocialnetworkgui/utils/CONSTANTS.java
@@ -2,14 +2,14 @@ package com.toysocialnetworkgui.utils;
 
 public class CONSTANTS {
 
-    public static final int MAIN_SCREEN_WIDTH = 1000;
-    public static final int MAIN_SCREEN_HEIGHT = 600;
+    public static final int MAIN_SCREEN_WIDTH = 1200;
+    public static final int MAIN_SCREEN_HEIGHT = 800;
 
     public static final int ADMIN_SCREEN_WIDTH = 600;
     public static final int ADMIN_SCREEN_HEIGHT = 500;
 
-    public static final int LOGIN_SCREEN_WIDTH = 400;
-    public static final int LOGIN_SCREEN_HEIGHT = 250;
+    public static final int LOGIN_SCREEN_WIDTH = 800;
+    public static final int LOGIN_SCREEN_HEIGHT = 600;
 
     public static final String DATETIME_PATTERN = "dd-MM-yyyy HH:mm";
 
