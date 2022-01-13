25e183444daa419d2322b5fb3e043eded3003f5b
StefanButacu <stefbutacu@gmail.com>
2022-01-13 23:13:22 +0200

    Skeleton friend report

--- src/main/java/com/toysocialnetworkgui/controller/FriendReportController.java
+++ src/main/java/com/toysocialnetworkgui/controller/FriendReportController.java
@@ -7,10 +7,14 @@ import com.toysocialnetworkgui.utils.UserMessageDTO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Side;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
@@ -21,7 +25,10 @@ import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.Month;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FriendReportController {
     @FXML
@@ -32,6 +39,19 @@ public class FriendReportController {
     Button buttonExport;
     @FXML
     ListView<UserMessageDTO> listViewMessages;
+
+    @FXML
+    PieChart pieChartMessages;
+
+    @FXML
+    Label caption;
+
+    @FXML
+    Label noMessages;
+    @FXML
+    VBox vboxChart;
+
+    int messagesInInterval;
 
     private Service service;
     private User loggedUser;
@@ -45,14 +65,50 @@ public class FriendReportController {
         this.rightPane = rightPane;
         labelTitle.setText("Conversation between " + loggedUser + " and " + otherUser);
         loadMessages(dateFrom, dateUntil);
+        populateAllTimeMessages();
+    }
+    private void populateAllTimeMessages() {
+
+//        List<UserMessageDTO> result = getConvDto(date)
+//        pieChartMessages.getData().add(new PieChart.Data("Messages in interval ", messagesInInterval ));
+//
+//        vboxChart.setVisible(monthsFrq.size() != 0);
+//        noMessages.setVisible(monthsFrq.size() == 0);
+
+        pieChartMessages.setAnimated(true);
+        pieChartMessages.setLabelsVisible(true);
+        pieChartMessages.setLabelLineLength(10);
+        pieChartMessages.setLegendSide(Side.LEFT);
+        for (final PieChart.Data data : pieChartMessages.getData()) {
+            data.getNode().addEventHandler(MouseEvent.MOUSE_ENTERED,
+                    e -> {
+                        String label = Math.round(data.getPieValue()) + " " + data.getName();
+                        if( Math.round(data.getPieValue()) == 1)
+                            label += " message";
+                        else
+                            label += " messages";
+                        caption.setText(label);
+                    });
+            data.getNode().addEventHandler(MouseEvent.MOUSE_EXITED,
+                    e -> {
+                        String label = "Hover slice to get detailed statistics!";
+                        caption.setText(label);
+                    });
+
+        }
     }
 
+    private List<UserMessageDTO> getConvDto(LocalDate dateFrom, LocalDate dateUntil){
+        List<UserMessageDTO> result =  service.getConversationUserMessageDTOs(List.of(loggedUser.getEmail(), otherUser.getEmail()))
+                .stream()
+                .filter(m -> !m.getDate().toLocalDate().isAfter(dateUntil) && !m.getDate().toLocalDate().isBefore(dateFrom))
+                .toList();
+        messagesInInterval = result.size();
+        return result;
+    }
     private void loadMessages(LocalDate dateFrom, LocalDate dateUntil) {
         listViewMessages.getItems()
-                .setAll(service.getConversationUserMessageDTOs(List.of(loggedUser.getEmail(), otherUser.getEmail()))
-                        .stream()
-                        .filter(m -> !m.getDate().toLocalDate().isAfter(dateUntil) && !m.getDate().toLocalDate().isBefore(dateFrom))
-                        .toList());
+                .setAll(getConvDto(dateFrom, dateUntil));
     }
 
     @FXML
@@ -107,99 +163,6 @@ public class FriendReportController {
 
         contentStream.endText();
         contentStream.close();
-     ////////////////////////////////////////////////////
-        // Create a new font object selecting one of the PDF base fonts
-//        PDFont fontPlain = PDType1Font.HELVETICA;
-//        PDFont fontBold = PDType1Font.HELVETICA_BOLD;
-//        PDFont fontItalic = PDType1Font.HELVETICA_OBLIQUE;
-//        PDFont fontMono = PDType1Font.COURIER;
-//
-//        // Create a document and add a page to it
-//    //    PDDocument document = new PDDocument();
-//        PDPage page = new PDPage(PDRectangle.A4);
-//        // PDRectangle.LETTER and others are also possible
-//        PDRectangle rect = page.getMediaBox();
-//        // rect can be used to get the page width and height
-//        document.addPage(page);
-//
-//        // Start a new content stream which will "hold" the to be created content
-//        PDPageContentStream cos = new PDPageContentStream(document, page);
-//
-//        //Dummy Table
-//        float margin = 50;
-//        // starting y position is whole page height subtracted by top and bottom margin
-//        float yStartNewPage = page.getMediaBox().getHeight() - (2 * margin);
-//        // we want table across whole page width (subtracted by left and right margin ofcourse)
-//        float tableWidth = page.getMediaBox().getWidth() - (2 * margin);
-//
-//        boolean drawContent = true;
-//        float yStart = yStartNewPage;
-//        float bottomMargin = 70;
-//        // y position is your coordinate of top left corner of the table
-//        float yPosition = 550;
-//
-//        BaseTable table = new BaseTable(yPosition, yStartNewPage,
-//                bottomMargin, tableWidth, margin, document, page, true, drawContent);
-//
-//        // the parameter is the row height
-//        Row<PDPage> headerRow = table.createRow(50);
-//        // the first parameter is the cell width
-//        Cell<PDPage> cell = headerRow.createCell(100, "Header");
-//        cell.setFont(fontBold);
-//        cell.setFontSize(20);
-//        // vertical alignment
-//        cell.setValign(VerticalAlignment.MIDDLE);
-//        // border style
-//        cell.setTopBorderStyle(new LineStyle(Color.BLACK, 10));
-//        table.addHeaderRow(headerRow);
-//
-//        Row<PDPage> row = table.createRow(20);
-//        cell = row.createCell(30, "black left plain");
-//        cell.setFontSize(15);
-//        cell = row.createCell(70, "black left bold");
-//        cell.setFontSize(15);
-//        cell.setFont(fontBold);
-//
-//        row = table.createRow(20);
-//        cell = row.createCell(50, "red right mono");
-//        cell.setTextColor(Color.RED);
-//        cell.setFontSize(15);
-//        cell.setFont(fontMono);
-//        // horizontal alignment
-//        cell.setAlign(HorizontalAlignment.RIGHT);
-//        cell.setBottomBorderStyle(new LineStyle(Color.RED, 5));
-//        cell = row.createCell(50, "green centered italic");
-//        cell.setTextColor(Color.GREEN);
-//        cell.setFontSize(15);
-//        cell.setFont(fontItalic);
-//        cell.setAlign(HorizontalAlignment.CENTER);
-//        cell.setBottomBorderStyle(new LineStyle(Color.GREEN, 5));
-//
-//        row = table.createRow(20);
-//        cell = row.createCell(40, "rotated");
-//        cell.setFontSize(15);
-//        // rotate the text
-//        cell.setTextRotated(true);
-//        cell.setAlign(HorizontalAlignment.RIGHT);
-//        cell.setValign(VerticalAlignment.MIDDLE);
-//        // long text that wraps
-//        cell = row.createCell(30, "long text long text long text long text long text long text long text");
-//        cell.setFontSize(12);
-//        // long text that wraps, with more line spacing
-//        cell = row.createCell(30, "long text long text long text long text long text long text long text");
-//        cell.setFontSize(12);
-//        cell.setLineSpacing(2);
-//
-//        table.draw();
-//
-//        float tableHeight = table.getHeaderAndDataHeight();
-//        System.out.println("tableHeight = "+tableHeight);
-//
-//        // close the content stream
-//        cos.close();
-//
-//        // Save the results and ensure that the document is properly closed:
-//
-//        document.close();
+
     }
 }
