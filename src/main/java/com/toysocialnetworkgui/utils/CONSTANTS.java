1483f83aeaab506a61d09736d1d19a0e70766145
eduardcadar <eduard.cadar@stud.ubbcluj.ro>
2022-01-14 23:24:10 +0200

    fixed login screen constants

--- src/main/java/com/toysocialnetworkgui/utils/CONSTANTS.java
+++ src/main/java/com/toysocialnetworkgui/utils/CONSTANTS.java
@@ -8,8 +8,8 @@ public class CONSTANTS {
     public static final int ADMIN_SCREEN_WIDTH = 600;
     public static final int ADMIN_SCREEN_HEIGHT = 500;
 
-    public static final int LOGIN_SCREEN_WIDTH = 620;
-    public static final int LOGIN_SCREEN_HEIGHT = 410;
+    public static final int LOGIN_SCREEN_WIDTH = 630;
+    public static final int LOGIN_SCREEN_HEIGHT = 412;
 
     public static final String DATETIME_PATTERN = "dd/MM/yyyy HH:mm";
     public static final String DATE_PATTERN = "dd/MM/yyyy";
