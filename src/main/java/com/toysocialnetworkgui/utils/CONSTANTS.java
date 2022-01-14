d6b3e7012793ede9b48506628e5a6758e58093de
StefanButacu <stefbutacu@gmail.com>
2022-01-14 15:46:30 +0200

    Hell

--- src/main/java/com/toysocialnetworkgui/utils/CONSTANTS.java
+++ src/main/java/com/toysocialnetworkgui/utils/CONSTANTS.java
@@ -2,7 +2,7 @@ package com.toysocialnetworkgui.utils;
 
 public class CONSTANTS {
 
-    public static final int MAIN_SCREEN_WIDTH = 1200;
+    public static final int MAIN_SCREEN_WIDTH = 1100;
     public static final int MAIN_SCREEN_HEIGHT = 650;
 
     public static final int ADMIN_SCREEN_WIDTH = 600;
